29d09abb98c2a26eeea28b6a41346f7e7a2a24a0
yashi <yashi@visionnav.com>
2022-11-28 10:53:48 +0800

    1

---
+++ demoForInterlockedConsole31/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace demoForInterlockedConsole31
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = 25;
+            Console.WriteLine($"origin age:{age}");
+            var data = Interlocked.Exchange(ref age, 0);
+            Console.WriteLine($"data result:{data}");
+            Console.WriteLine($"new age:{age}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- demoForPortConsole31/Program.cs
+++ demoForPortConsole31/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Threading;
 
 namespace demoForPortConsole31
 {
